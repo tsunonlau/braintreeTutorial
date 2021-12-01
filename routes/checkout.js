88972da17003df8434709ffd20103375273573ab
tsunonlau <tsunonlau@gmail.com>
2021-12-01 16:38:07 +0800

    init

---
+++ routes/checkout.js
@@ -0,0 +1,34 @@
+const express = require('express');
+const router = express.Router();
+const braintree = require('braintree');
+
+router.post('/', (req, res, next) => {
+  const gateway = new braintree.BraintreeGateway({
+    environment: braintree.Environment.Sandbox,
+    // Use your own credentials from the sandbox Control Panel here
+    merchantId:   'cd8s96zvwrn2cghw',
+    publicKey:    'p47mcfmh9h64kbdk', //encrypt this in prod.
+    privateKey:   '478285a257218151a2ebb752eca42b27' //encrypt this in prod.
+  });
+
+  // Use the payment method nonce here
+  const nonceFromTheClient = req.body.paymentMethodNonce;
+  // Create a new transaction for $10
+  const newTransaction = gateway.transaction.sale({
+    amount: '10.00',
+    paymentMethodNonce: nonceFromTheClient,
+    options: {
+      // This option requests the funds from the transaction
+      // once it has been authorized successfully
+      submitForSettlement: true
+    }
+  }, (error, result) => {
+      if (result) {
+        res.send(result);
+      } else {
+        res.status(500).send(error);
+      }
+  });
+});
+
+module.exports = router;
